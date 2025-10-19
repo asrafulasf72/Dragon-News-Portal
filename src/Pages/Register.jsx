6de3fef2b02ffc70aeef45fc8519a86eb1bd2da8
asraful <asrafulislamasf@gmail.com>
2025-10-20 02:27:50 +0600

    OnAuthStateChange and Register User Functionality added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../Provider/AuthProvider';
+import { AlertTriangle } from 'lucide-react';
 
 const Register = () => {
+  const {creatUser, setUser}=use(AuthContext)
+  const handelRegister=(e)=>{
+    e.preventDefault();
+    const form=e.target
+    const name=form.name.value;
+    const photo=form.photo.value;
+    const email=form.email.value;
+    const password=form.password.value;
+    // console.log("The Form Value Is: ",{name,photo,email,password})
+
+    creatUser(email,password)
+    .then((res)=>{
+       const user=res.user;
+       setUser(user)
+    })
+    .catch((error)=>{
+        const errormessage=error.message
+        alert(`${errormessage}`)
+    })
+  }
     return (
          <div className='flex justify-center items-center h-screen'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
               <div className="card-body">
                 <h1 className='text-center text-[2rem] font-[600px] my-5'>Register your account</h1>
-                <form>
+                <form onSubmit={handelRegister}>
                     <fieldset className="fieldset">
                          
                         <label className="label">Name</label>
-                       <input type="text" className="input bg-base-200" placeholder="Enter Your Name" />
+                       <input type="text" name='name' className="input bg-base-200" placeholder="Enter Your Name" required />
 
                        {/* Photo URL */}
                         <label className="label">Photo URL</label>
-                       <input type="text" className="input bg-base-200" placeholder="Enter Your Photo URL" />
+                       <input type="text" name='photo' className="input bg-base-200" placeholder="Enter Your Photo URL" required />
 
                         {/* Email */}
                        <label className="label">Email</label>
-                       <input type="email" className="input bg-base-200" placeholder="Enter Your Email" />
+                       <input type="email" name='email' className="input bg-base-200" placeholder="Enter Your Email" required />
                         
                         {/* Password */}
                        <label className="label">Password</label>
-                       <input  type="password" className="input bg-base-200" placeholder="Enter your Password" />
+                       <input  type="password" name='password' className="input bg-base-200" placeholder="Enter your Password" required />
 
-                       <button className="btn btn-neutral mt-4">Register</button>
+                       <button type='submit' className="btn btn-neutral mt-4">Register</button>
                    </fieldset>
                 </form>
                 <p>Already Have An Account ? <Link className='text-secondary' to='/auth/login'>Login</Link></p>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,29 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import app from '../Firebase/Firebase.init';
 
 export const AuthContext=createContext();
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
+    console.log(user)
+
+    const creatUser=(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    useEffect(()=>{
+        const unsubScrib=onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser)
+        })
+        return ()=>{
+            unsubScrib();
+        }
+    },[])
     const AuthData={
         user,
-        setUser
+        setUser,
+        creatUser
     }
     return <AuthContext value={AuthData}>{children}</AuthContext>;
 };
